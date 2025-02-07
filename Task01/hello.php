7ffcae8b569f80e24aa43132be1dcd1727572c64
werrys302 <werrys302>
2025-02-07 20:28:10 +0300

    Task01

---
+++ Task01/hello.php
@@ -0,0 +1,8 @@
+<?php
+    date_default_timezone_set('Europe/Moscow');
+
+    $time = date('Y-m-d H:i:s');
+    $name = 'Уренков Юрий';
+
+    echo "Текущая дата и время: $time ";
+    echo "Фамилия и имя: $name";
